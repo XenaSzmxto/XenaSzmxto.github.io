06e0629e1904816f1ec2557eef7508a554f5a12a
XenaSzmxto <82598198+XenaSzmxto@users.noreply.github.com>
2022-11-18 11:55:06 +0100

    Add files via upload

---
+++ skrypty.js
@@ -0,0 +1,28 @@
+function Czyszczenie() {
+    document.getElementById("result").value = "";
+}
+ 
+function wyswietl(value) {
+    document.getElementById("result").value += value;
+}
+ 
+ 
+function oblicz() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+ 
+function dodajRamke(){
+document.getElementById("calosc").style.backgroundColor="yellow";
+}
+
+
+
+
+
+
+
+
+
+
